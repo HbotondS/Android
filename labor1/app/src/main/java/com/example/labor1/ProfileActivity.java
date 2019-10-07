4a6dc551fa8a4f7978efced9c10e40ef17a05a67
Botond Hegyi <hbotond97@gmail.com>
2019-10-08 00:05:15 +0300

    labor1: start implementing counter

--- labor1/app/src/main/java/com/example/labor1/ProfileActivity.java
+++ labor1/app/src/main/java/com/example/labor1/ProfileActivity.java
@@ -7,17 +7,31 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    private static int callFromLogin = 0;
+    private static int callFromRegister = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        if (getIntent().getExtras() != null) {
-            ((TextView) this.findViewById(R.id.firstNameText)).setText(getIntent().getExtras().getString("firstName"));
-            ((TextView) this.findViewById(R.id.lastNameText)).setText(getIntent().getExtras().getString("lastName"));
-            ((TextView) this.findViewById(R.id.departmentText)).setText(getIntent().getExtras().getString("department"));
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            ((TextView) this.findViewById(R.id.firstNameText)).setText(bundle.getString("firstName"));
+            ((TextView) this.findViewById(R.id.lastNameText)).setText(bundle.getString("lastName"));
+            ((TextView) this.findViewById(R.id.departmentText)).setText(bundle.getString("department"));
+            String caller = bundle.getString("caller");
+            if (Objects.equals(caller, "login")) {
+                callFromLogin++;
+            } else if (Objects.equals(caller, "register")) {
+                callFromRegister++;
+            }
         }
+        ((TextView) this.findViewById(R.id.loginCallCounter)).setText(String.valueOf(callFromLogin));
+        ((TextView) this.findViewById(R.id.registerCallCounter)).setText(String.valueOf(callFromRegister));
     }
 
     public void callSignIn(View view) {
--- labor1/app/src/main/java/com/example/labor1/registerActivity.java
+++ labor1/app/src/main/java/com/example/labor1/registerActivity.java
@@ -4,22 +4,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+
 public class registerActivity extends AppCompatActivity {
+
+    private static int callFromProfile = 0;
+    private static int callFromLogin = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            String caller = bundle.getString("caller");
+            if (Objects.equals(caller, "profile")) {
+                callFromProfile++;
+            } else if (Objects.equals(caller, "login")) {
+                callFromLogin++;
+            }
+        }
+        ((TextView) this.findViewById(R.id.profileCallCounter)).setText(String.valueOf(callFromProfile));
+        ((TextView) this.findViewById(R.id.loginCallCounter)).setText(String.valueOf(callFromLogin));
     }
 
     public void callProfile(View view) {
         Intent profileActivity = new Intent(this, ProfileActivity.class)
                 .putExtra("firstName", ((EditText) this.findViewById(R.id.firstNameText)).getText().toString())
                 .putExtra("lastName", ((EditText) this.findViewById(R.id.lastNameText)).getText().toString())
-                .putExtra("department", ((EditText) this.findViewById(R.id.departmentText)).getText().toString());
+                .putExtra("department", ((EditText) this.findViewById(R.id.departmentText)).getText().toString())
+                .putExtra("caller", "register");
         startActivity(profileActivity);
     }
 }
