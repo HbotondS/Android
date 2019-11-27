4a2bc4cdeb6cf8c15b7d308e55c5cfca3be70b06
Botond Hegyi <hbotond97@gmail.com>
2019-11-27 19:01:30 +0200

    project1: admin -> fix bug

--- project_1/admin/app/src/main/java/com/example/admin/MainActivity.kt
+++ project_1/admin/app/src/main/java/com/example/admin/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Utils.startFragment(supportFragmentManager, R.id.layoutHolder, LoginFragment())
+        supportFragmentManager.beginTransaction().add(R.id.layoutHolder, LoginFragment()).commit()
     }
 }
