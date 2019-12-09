b16bd6d0d6ecf92e11cb28b4bc15809502ff9615
Botond Hegyi <hbotond97@gmail.com>
2019-12-09 21:07:39 +0200

    project2: create some of the views

---
+++ project_2/MovieDB/app/src/main/java/com/example/moviedb/NavigationFragment.kt
@@ -0,0 +1,27 @@
+package com.example.moviedb
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class NavigationFragment : Fragment() {
+
+    private val TAG = "NavigationFragment"
+
+    private lateinit var myView: View
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "view created")
+        myView = inflater.inflate(R.layout.navigation_bar, container, false)
+
+        return myView
+    }
+
+}
---
+++ project_2/MovieDB/app/src/main/java/com/example/moviedb/ProfileFragment.kt
@@ -0,0 +1,26 @@
+package com.example.moviedb
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ProfileFragment : Fragment() {
+
+    private val TAG = "ProfileFragment"
+
+    private lateinit var myView: View
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "view created")
+        myView = inflater.inflate(R.layout.profile_layout, container, false)
+
+        return myView
+    }
+}
